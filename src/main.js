41d72a77444580d34ad2b4d1fde3133d0948434b
KhashayarKhm <khashayar.khm@protonmail.com>
2021-10-02 19:20:18 +0330

    Remove bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,15 +1,10 @@
 import Vue from 'vue';
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import App from './App.vue';
 
 // Import animate.css
 import 'animate.css';
 
 import vuetify from './plugins/vuetify';
-
-// Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue);
-Vue.use(IconsPlugin);
 
 Vue.config.productionTip = false;
 
