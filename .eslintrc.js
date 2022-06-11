26c03a3e4d36d46eb92845eb4b5fe5eb1c65b291
KhashayarKhm <khashayar.khm@protonmail.com>
2022-06-11 22:30:28 +0430

    refactor the eslintrc file

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+	root: true,
+	env: {
+		browser: true,
+		es6: true,
+	},
+	extends: [
+		'plugin:vue/essential',
+		'plugin:vue/strongly-recommended',
+		'plugin:vue/recommended',
+		'plugin:import/recommended',
+		'plugin:import/warnings',
+		'airbnb-base',
+	],
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly',
+	},
+	parserOptions: {
+		ecmaVersion: 2018,
+		parser: 'babel-eslint',
+		sourceType: 'module',
+	},
+	plugins: ['vue'],
+	rules: {
+		'no-console': 'off',
+		indent: [2, 'tab', { SwitchCase: 1 }],
+		'no-tabs': 0,
+		'vue/html-indent': [2, 'tab'],
+		'no-underscore-dangle': ['error', { allow: ['_id'] }],
+		'object-shorthand': ['error', 'properties'],
+		'no-param-reassign': ['error', {
+			props: true,
+			ignorePropertyModificationsFor: ['state'],
+		}],
+	},
+	settings: {
+		'import/resolver': {
+			'eslint-import-resolver-custom-alias': {
+				alias: {
+					'@': './src',
+				},
+				extensions: ['.js'],
+			},
+		},
+	},
+};
